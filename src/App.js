0effe1ed088f23335c3595539245a3bb8b1035db
Fardin Mohammed <fardeenmohdpl@gmail.com>
2021-08-17 20:26:25 +0200

    Add movie detail page and movie state

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Nav from "./components/Nav";
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
-
+import MovieDetail from "./pages/MovieDetail";
 //Router Stuff
 import { Switch, Route } from "react-router-dom";
 function App() {
@@ -21,8 +21,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
         </Route>
         <Route path="/contact">
           <ContactUs />s
