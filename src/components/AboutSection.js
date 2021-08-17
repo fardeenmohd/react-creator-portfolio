516542b6e2ce194da4dd43a4351baaa1d4255431
Fardin Mohammed <fardeenmohdpl@gmail.com>
2021-08-18 01:09:09 +0200

    Add wave animation on about us landing page

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,9 +1,10 @@
 import React from "react";
 import home1 from "../img/home1.png";
 import { About, Description, Image, Hide } from "../styles";
-
+import Wave from "./Wave";
 //Framer Motion
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
 
 const AboutSection = () => {
   return (
@@ -11,26 +12,27 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. we
           have professionals with amazing sills
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="" />
+        <motion.img variants={photoAnim} src={home1} alt="" />
       </Image>
+      <Wave />
     </About>
   );
 };
