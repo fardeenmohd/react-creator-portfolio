457094782cb47220501da9e67b87b3b7841daee9
Fardin Mohammed <fardeenmohdpl@gmail.com>
2021-08-17 19:28:07 +0200

    Add React Router to the app

--- src/App.js
+++ src/App.js
@@ -7,13 +7,27 @@ import Nav from "./components/Nav";
 
 //Import Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 
+//Router Stuff
+import { Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />s
+        </Route>
+      </Switch>
     </div>
   );
 }
