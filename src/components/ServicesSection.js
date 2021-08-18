53aed3ee16244e9932e0422556f95828c4284bfc
Fardin Mohammed <fardeenmohdpl@gmail.com>
2021-08-18 03:09:38 +0200

    Add scroll animation using a custom Hook

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+
 import clock from "../img/clock.svg";
 import diaphragm from "../img/diaphragm.svg";
 import money from "../img/money.svg";
@@ -7,10 +8,19 @@ import teamwork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
 
 import { About, Description, Image } from "../styles";
+import { scrollReveal } from "../animation";
+import { useScroll } from "./useScroll";
 
 const ServicesSection = () => {
+  const [element, controls] = useScroll();
+
   return (
-    <Services>
+    <Services
+      variants={scrollReveal}
+      animate={controls}
+      initial="hidden"
+      ref={element}
+    >
       <Description>
         <h2>
           High <span>quality</span> services
